e155cbff004db9032eb4ca134e7948720b4cfa88
zakstone7 <30512830+zakstone7@users.noreply.github.com>
2021-09-29 12:47:04 -0400

    Update getPicks.js

--- getPicks.js
+++ getPicks.js
@@ -1,5 +1,6 @@
 var games = [];
 var gamesTables = document.getElementById('nflheader').querySelectorAll('table.gameScore');
+var weekNumber = document.getElementById('fantasyPageHeaderDynamic').querySelector('.filters form > div > div > span').innerText;
 
 for (var i = 0; i < gamesTables.length; i++){
 	var cells = gamesTables[i].querySelectorAll('td');
@@ -16,6 +17,7 @@ function parsePick(text) {
 }
 
 var picks = [];
+var ytd = [];
 
 var table = document.getElementById('nflplayerRows');
 var pickRows = table.querySelectorAll('tr.bg2, tr.bgFan');
@@ -25,6 +27,7 @@ for (var i = 0; i < pickRows.length; i++) {
 	var cells = row.querySelectorAll('td')
 	var name = cells[0].innerText;
 	var pick = { "name": name, 'games': [] };
+	ytd.push({ "name": name, "total": parseInt(cells[cells.length-1].innerText)});
 
 	for (var j = 1; j < cells.length - 3; j++) {
 		var parsedPick = parsePick(cells[j].innerText)
@@ -34,5 +37,14 @@ for (var i = 0; i < pickRows.length; i++) {
 	picks.push(pick);
 }
 
-console.log(JSON.stringify(games));
-console.log(JSON.stringify(picks));
+console.log(`var week${parseInt(weekNumber) + 1}ytd = ${JSON.stringify(ytd)};`)
+
+var javascriptWeekString = 
+`var week${weekNumber}Games = ${JSON.stringify(games)};
+var week${weekNumber}Picks = ${JSON.stringify(picks)};
+var week${weekNumber}Winners = [];
+var week${weekNumber}Ties = [];
+
+weeks.push({ 'games': week${weekNumber}Games, 'picks': week${weekNumber}Picks, 'winners': week${weekNumber}Winners, 'ties': week${weekNumber}Ties, 'ytd': week${weekNumber}ytd });`
+
+console.log(javascriptWeekString);
